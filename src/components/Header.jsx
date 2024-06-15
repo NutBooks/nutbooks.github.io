12de528c7cb291665d91ead9a633efbb5e5bff6a
cheesecat47 <cheesecat47@gmail.com>
2024-06-15 18:17:30 +0900

    feat: add logo image in navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom';
-import nutbooks_logo from '../assets/nutbooks_logo.svg';
+import LogoLight from '../assets/nutbooks_logo.svg?react';
+import LogoDark from '../assets/nutbooks_logo_dark.svg?react';
 
 const Header = () => {
   const navigate = useNavigate();
@@ -9,8 +10,9 @@ const Header = () => {
       <div className="navbar">
         <div className="navbar-start">
           <div className="flex items-center" onClick={() => navigate('/')}>
-            <img className="w-8" src={nutbooks_logo} alt="logo" />
-            <span className="font-serif text-2xl">NutBooks</span>
+            <LogoLight className="h-8 w-9 dark:hidden" />
+            <LogoDark className="hidden h-8 w-9 dark:block" />
+            <span className="font-serif text-2xl dark:text-white">NutBooks</span>
           </div>
         </div>
         <div className="navbar-center">
