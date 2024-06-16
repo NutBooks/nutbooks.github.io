287df8008b9bb6e679df21494436094b06f36b34
cheesecat47 <cheesecat47@gmail.com>
2024-06-16 14:21:19 +0900

    feat: fetch article list and show on table

--- src/components/BookmarkList.jsx
+++ src/components/BookmarkList.jsx
@@ -2,13 +2,11 @@ import { useEffect, useState } from 'react';
 import { axiosWithCredentialInstance } from '../apis/axiosInstance';
 
 const BookmarkList = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [articleList, setArticleList] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    try {
-      setLoading(true);
+    const fetchData = () => {
       axiosWithCredentialInstance
         .get('/article', {
           params: {
@@ -17,15 +15,64 @@ const BookmarkList = () => {
           },
         })
         .then(response => {
-          setArticleList(response.data.data.content);
-          console.table(articleList);
+          setData(response.data.data.content);
+          setLoading(false);
+        })
+        .catch(() => {
+          setLoading(false);
         });
-    } catch (e) {
-      setError(e);
-    }
-  }, [articleList]);
+    };
 
-  return <></>;
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="rounded-lg bg-white p-4">
+        <h2 className="text-lg">저장된 북마크 목록</h2>
+        <div>Loading ...</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="rounded-lg bg-white p-4">
+      <h2 className="text-lg">저장된 북마크 목록</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Keywords</th>
+              <th>Tags</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(article => (
+              <tr key={article.id} className="hover">
+                <th>{article.id}</th>
+                <td>{article.title}</td>
+                <td>{article.author}</td>
+                <td>{article.keywords}</td>
+                <td className="flex gap-2">
+                  {article.tags &&
+                    article.tags.map(tag => (
+                      <span key={tag.index} className="rounded bg-lime-600 p-1 text-white">
+                        {tag}
+                      </span>
+                    ))}
+                </td>
+                <td>{article.text ? 'Done' : 'Queued'}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default BookmarkList;
