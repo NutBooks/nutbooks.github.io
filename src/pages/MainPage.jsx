6b61fd69fad4f9610c9759dd62c1abfb36249832
cheesecat47 <cheesecat47@gmail.com>
2024-06-19 13:58:39 +0900

    feat: add main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import mainGif from '../assets/main.gif';
 import Header from '../components/Header.jsx';
 
 const MainPage = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Header />
+      <div className="hero min-h-[calc(100vh-72px)]">
+        <div className="hero-content flex-col gap-8 lg:flex-row">
+          <img src={mainGif} className="max-w-full rounded-lg shadow-2xl sm:max-w-sm" />
+          <div>
+            <h1 className="text-4xl font-bold">핵심만 읽어보세요!</h1>
+            <p className="py-6">
+              &quot;다음에 읽어야지!&quot; 하며 저장했지만 읽지 않은 채 너무 많아진 북마크,
+              <br />
+              핵심만 요약해서 알려드릴게요!
+            </p>
+            <button type="button" className="btn btn-primary" onClick={() => navigate('/list')}>
+              시작하기
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
